87272f69d9eec8c0f8725e8b8509564284521d8b
Hunter Kemeny <alek.kemeny@gmail.com>
2024-01-11 21:10:14 -0700

    Add license information

--- benchmarking/benchmark.py
+++ benchmarking/benchmark.py
@@ -1,3 +1,54 @@
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+# QASMBench Benchmark Suite:
+# A low-level OpenQASM benchmark suite for NISQ evaluation and simulation.
+
+# 06/04/2020 by Ang Li from High-Performance-Computing Group,
+# ACMD, PCSD, Pacific Northwest National Laboratory (PNNL),
+# Richland, WA, 99354, USA.
+
+
+# Copyright © 2020, Battelle Memorial Institute
+
+# 1.Battelle Memorial Institute (hereinafter Battelle) hereby grants permission 
+# to any person or entity lawfully obtaining a copy of this software and associated
+# documentation files (hereinafter “the Software”) to redistribute and use the 
+# Software in source and binary forms, with or without modification.  Such person 
+# or entity may use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and may permit others to do so, subject to the 
+# following conditions:
+
+# - Redistributions of source code must retain the above copyright notice, this list 
+# of conditions and the following disclaimers. 
+
+# - Redistributions in binary form must reproduce the above copyright notice, this list 
+# of conditions and the following disclaimer in the documentation and/or other materials 
+# provided with the distribution. 
+
+# - Other than as used herein, neither the name Battelle Memorial Institute or Battelle 
+# may be used in any form whatsoever without the express written consent of Battelle.  
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+# SHALL BATTELLE OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# This code is from QASMBench's QMetric.py module and adapted to work with red-queen.
+# The original source can be found at:
+#
+# https://github.com/pnnl/QASMBench/blob/master/metrics/QMetric.py
+
 import numpy as np
 import qiskit
 from qiskit.compiler import *
